9c063dfbe0aa94bbad5253f93573128fca608e5a
Jouin Alexis <alexis.jouin@live.fr>
2016-11-29 13:32:51 +0100

    First commit => TP4 STATUS BAR en cours de dév !

---
+++ tp4/Drawing/src/drawing/Paint.java
@@ -0,0 +1,69 @@
+package drawing;
+
+import java.awt.BorderLayout;
+import java.awt.Color;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+/**
+ * Classe Interface graphique pour l'application de dessin
+ */
+public class Paint {
+
+	private JFrame frame;
+	private JButton clearButton;
+	private JButton circleButton;
+	private JButton rectangleButton;
+	private JButton testButton;
+	private JPanel buttonPanel;
+	private JPanel mainPanel;
+	private Drawing drawing;
+	private StatusBar statusBar;
+	
+	public void run(){
+		frame = new JFrame("Paint");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		mainPanel = new JPanel(new BorderLayout());
+		
+		drawing = new Drawing();
+		drawing.setBackground(Color.WHITE);
+		clearButton = new JButton("Clear");
+		circleButton = new JButton("Circle");
+		rectangleButton = new JButton("Rectangle");
+		testButton = new JButton("Test");
+		
+		statusBar = new StatusBar();
+		
+		
+		buttonPanel = new JPanel();
+		buttonPanel.add(clearButton);
+		buttonPanel.add(circleButton);
+		buttonPanel.add(rectangleButton);
+		buttonPanel.add(testButton);
+		
+		mainPanel.add(buttonPanel, BorderLayout.SOUTH);
+		mainPanel.add(drawing, BorderLayout.CENTER);
+		
+		//listeners pour les boutons
+		clearButton.addActionListener(new ClearButtonListener(drawing));
+		circleButton.addActionListener(new CircleButtonListener(drawing));
+		rectangleButton.addActionListener(new RectangleButtonListener(drawing));
+		
+		//listeners pour la zone de dessin
+		DrawingMouseListener l = new DrawingMouseListener(drawing);
+		drawing.addMouseListener(l);
+		drawing.addMouseMotionListener(l);
+
+		frame.getContentPane().add(mainPanel);
+		frame.setSize(640,480);
+		frame.setVisible(true);
+	}
+	
+	
+	public static void main(String[] args){
+		Paint app = new Paint();
+		app.run();
+	}
+}
---
+++ tp4/Drawing/src/drawing/StatusBar.java
@@ -0,0 +1,17 @@
+package drawing;
+
+import javax.swing.*;
+
+public class StatusBar extends JPanel {
+
+	private int nbFigure;
+	private int nbCircle;
+	private int nbRectangle;
+	
+	public StatusBar(){
+		super();
+		
+		
+	}
+	
+}
